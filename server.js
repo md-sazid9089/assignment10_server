d93d6a8dbdc4b0d009d7dfc5995d1e39d0dbfd06
md-sazid9089 <mdsazid9089@gmail.com>
2025-11-24 03:06:35 +0600

    struggling

--- server.js
+++ server.js
@@ -32,12 +32,10 @@ app.use(cookieParser());
 
 // Import routes
 const artworkRoutes = require('./routes/artworkRoutes');
-const favoriteRoutes = require('./routes/favoriteRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 // Mount routes
 app.use('/api/artworks', artworkRoutes);
-app.use('/api/favorites', favoriteRoutes);
 app.use('/api/users', userRoutes);
 
 // Root route
