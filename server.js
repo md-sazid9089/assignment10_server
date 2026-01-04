52cfe96302c1f19a85247b4b5a34d931cca31d8a
md-sazid9089 <mdsazid9089@gmail.com>
2026-01-05 00:20:20 +0600

    added

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
+  'http://localhost:5176',
   'http://localhost:3000',
   'https://artifyclient.netlify.app',
   process.env.CLIENT_URL,
